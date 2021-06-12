86f6d0e70c59ddb4e3b29c5cf269a9a4f9ee10cb
Daniela Solis B <dasolis506@gmail.com>
2021-06-11 22:54:24 -0600

    Galeria con botones

--- scripts/miscript.js
+++ scripts/miscript.js
@@ -3,12 +3,41 @@ function toggleBloque(){
        var mastexto= document.getElementById("mastexto"); 
     
     if (mastexto.style.display === "none"){
-        demo.textContent = "mostrar menos  ";
+        demo.textContent="mostrar menos";
         mastexto.style.display="block";
 
     }else{
-        demo.textContent = "Mostrar más ";
+        demo.textContent="Mostrar más";
         mastexto.style.display="none";
     }
+};
 
-}
+function toggleMascotas(){
+    var hobbies_galeria=document.getElementById("hobbies_galeria"); 
+    var mascotas_galeria=document.getElementById("mascotas_galeria"); 
+    var amigos_galeria=document.getElementById("amigos_galeria"); 
+    
+    amigos_galeria.style.display="none";
+    mascotas_galeria.style.display="block";
+    hobbies_galeria.style.display="none";
+};
+
+function toggleAmigos(){
+    var hobbies_galeria=document.getElementById("hobbies_galeria"); 
+    var mascotas_galeria=document.getElementById("mascotas_galeria"); 
+    var amigos_galeria=document.getElementById("amigos_galeria"); 
+    
+    amigos_galeria.style.display="block";
+    mascotas_galeria.style.display="none";
+    hobbies_galeria.style.display="none";
+};
+
+function toggleHobbies(){
+    var hobbies_galeria=document.getElementById("hobbies_galeria"); 
+    var mascotas_galeria=document.getElementById("mascotas_galeria"); 
+    var amigos_galeria=document.getElementById("amigos_galeria"); 
+    
+    amigos_galeria.style.display="none";
+    mascotas_galeria.style.display="none";
+    hobbies_galeria.style.display="block";
+};
