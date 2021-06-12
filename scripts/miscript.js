62ff9a7735aeb7de6629925b22924110709d2c36
Daniela Solis B <dasolis506@gmail.com>
2021-06-11 23:26:07 -0600

    entrega final

--- scripts/miscript.js
+++ scripts/miscript.js
@@ -13,31 +13,53 @@ function toggleBloque(){
 };
 
 function toggleMascotas(){
+
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var mascotas_galeria=document.getElementById("mascotas_galeria"); 
     var amigos_galeria=document.getElementById("amigos_galeria"); 
+    var a=document.getElementById("A"); 
+    var h=document.getElementById("H"); 
+    var m=document.getElementById("M"); 
     
+    mascotas_galeria.style.display="block";
+    m.style.backgroundColor="#AA0000";
+    a.style.backgroundColor="#FFFFFF";
+    h.style.backgroundColor="#FFFFFF";
+
     amigos_galeria.style.display="none";
     mascotas_galeria.style.display="block";
     hobbies_galeria.style.display="none";
+
 };
 
 function toggleAmigos(){
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var mascotas_galeria=document.getElementById("mascotas_galeria"); 
     var amigos_galeria=document.getElementById("amigos_galeria"); 
+    var a=document.getElementById("A"); 
+    var h=document.getElementById("H"); 
+    var m=document.getElementById("M"); 
     
     amigos_galeria.style.display="block";
     mascotas_galeria.style.display="none";
     hobbies_galeria.style.display="none";
+    a.style.backgroundColor="#AA0000";
+    h.style.backgroundColor="#FFFFFF";
+    m.style.backgroundColor="#FFFFFF";
 };
 
 function toggleHobbies(){
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var mascotas_galeria=document.getElementById("mascotas_galeria"); 
     var amigos_galeria=document.getElementById("amigos_galeria"); 
+    var a=document.getElementById("A"); 
+    var h=document.getElementById("H"); 
+    var m=document.getElementById("M"); 
     
     amigos_galeria.style.display="none";
     mascotas_galeria.style.display="none";
     hobbies_galeria.style.display="block";
+    h.style.backgroundColor="#AA0000";
+    a.style.backgroundColor="#FFFFFF";
+    m.style.backgroundColor="#FFFFFF";
 };
